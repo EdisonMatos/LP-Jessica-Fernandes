8e598b620aff01a4105c6c10562c9a39c4156031
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-08 17:04:55 -0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
-function nonBlockingCssPlugin() {
-  return {
-    name: 'non-blocking-css',
-    enforce: 'post',
-    transformIndexHtml(html) {
-      return html.replace(
-        /<link rel="stylesheet"([^>]+)>/g,
-        `<link rel="stylesheet"$1 media="print" onload="this.media='all'">`
-      )
-    },
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nonBlockingCssPlugin()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['primereact/dialog', 'primereact/button'],
   },
 })
