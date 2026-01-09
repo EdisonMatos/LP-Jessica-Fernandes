cf23793bdaea8e1c002657602d3693a65c518df8
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-09 08:52:58 -0300

    fix: Remove motion

--- src/components/sectionElements/hero/Influencer.jsx
+++ src/components/sectionElements/hero/Influencer.jsx
@@ -27,25 +27,22 @@ export default function Influencer({ appDownloadButtons }) {
               <div className="h-[50px] desktop1:h-[140px]" />
               <div className="flex flex-col-reverse gap-[10px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[40px] desktop1:pt-[0px] desktop1:pb-[0px]">
                 <div className="flex  flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]  ">
-                  <MotionDivDownToUp>
-                    <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
-                      <p className="mb-[16px] bg-minititle text-labelButtons bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
-                        {content.texts.hero.miniTag}
-                      </p>
-                    </div>
-                  </MotionDivDownToUp>
-                  <MotionDivDownToUp>
-                    <div className="text-lighter flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
-                      <h1 className="">{content.texts.hero.title}</h1>
-                    </div>
-                  </MotionDivDownToUp>
-                  <MotionDivDownToUp>
-                    <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
-                      <p className="text-lighter mb-[32px] ">
-                        {content.texts.hero.subtitle}
-                      </p>
-                    </div>
-                  </MotionDivDownToUp>
+                  <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
+                    <p className="mb-[16px] bg-minititle text-labelButtons bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
+                      {content.texts.hero.miniTag}
+                    </p>
+                  </div>
+
+                  <div className="text-lighter flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
+                    <h1 className="">{content.texts.hero.title}</h1>
+                  </div>
+
+                  <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
+                    <p className="text-lighter mb-[32px] ">
+                      {content.texts.hero.subtitle}
+                    </p>
+                  </div>
+
                   <div className="w-full phone2:w-full tablet2:w-auto">
                     <div className="flex justify-center w-full desktop1:justify-start mb-[32px]">
                       <Button
@@ -56,17 +53,17 @@ export default function Influencer({ appDownloadButtons }) {
                         icon={<FaWhatsapp size={24} />}
                       />
                     </div>
-                    <MotionDivDownToUp>
-                      <div className="flex justify-center desktop1:justify-start pb-[36px]">
-                        <div className="flex flex-col items-center desktop1:flex-row text-primary">
-                          {content.texts.hero.obsHero.icon}
-                          <p className="text-lighter  ml-[10px] text-center mt-[12px] desktop1:mt-0">
-                            {content.texts.hero.obsHero.text}
-                          </p>
-                        </div>
+
+                    <div className="flex justify-center desktop1:justify-start pb-[36px]">
+                      <div className="flex flex-col items-center desktop1:flex-row text-primary">
+                        {content.texts.hero.obsHero.icon}
+                        <p className="text-lighter  ml-[10px] text-center mt-[12px] desktop1:mt-0">
+                          {content.texts.hero.obsHero.text}
+                        </p>
                       </div>
-                    </MotionDivDownToUp>
-                    {appDownloadButtons && (
+                    </div>
+
+                    {/* {appDownloadButtons && (
                       <MotionDivDownToUp>
                         <div className="flex flex-col items-center desktop1:items-start">
                           <p className="mb-[20px] mt-[60px] text-labelButtons  text-center">
@@ -107,18 +104,18 @@ export default function Influencer({ appDownloadButtons }) {
                           </div>
                         </div>
                       </MotionDivDownToUp>
-                    )}
+                    )} */}
                   </div>
                 </div>
                 <div className="flex justify-center w-full tablet1:w-full desktop1:w-[35%] ">
-                  <MotionDivRightToLeft className="relative flex items-end justify-center w-full desktop2:justify-end">
+                  <div className="relative flex items-end justify-center w-full desktop2:justify-end">
                     <img
                       src="./personImg.webp"
                       className="h-[100%] relative desktop1:hidden"
                       alt="Foto do advogado"
                       fetchPriority="high"
                     ></img>
-                  </MotionDivRightToLeft>
+                  </div>
                 </div>
               </div>
             </div>
@@ -140,25 +137,22 @@ export default function Influencer({ appDownloadButtons }) {
                   <div className="h-[100px] desktop1:h-[140px]" />
                   <div className="flex flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between w-full items-center pb-[64px] pt-[40px] desktop1:pt-[0px] desktop1:pb-[0px]">
                     <div className="flex flex-col w-full desktop1:w-[50%] ">
-                      <MotionDivDownToUp>
-                        <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
-                          <p className="mb-[16px] bg-minititle text-labelButtons bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
-                            {content.texts.hero.miniTag}
-                          </p>
-                        </div>
-                      </MotionDivDownToUp>
-                      <MotionDivDownToUp>
-                        <div className="text-lighter flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
-                          <h1 className="">{content.texts.hero.title}</h1>
-                        </div>
-                      </MotionDivDownToUp>
-                      <MotionDivDownToUp>
-                        <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
-                          <p className="text-lighter mb-[32px] ">
-                            {content.texts.hero.subtitle}
-                          </p>
-                        </div>
-                      </MotionDivDownToUp>
+                      <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
+                        <p className="mb-[16px] bg-minititle text-labelButtons bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
+                          {content.texts.hero.miniTag}
+                        </p>
+                      </div>
+
+                      <div className="text-lighter flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
+                        <h1 className="">{content.texts.hero.title}</h1>
+                      </div>
+
+                      <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
+                        <p className="text-lighter mb-[32px] ">
+                          {content.texts.hero.subtitle}
+                        </p>
+                      </div>
+
                       <div className="w-full phone2:w-full tablet2:w-auto">
                         <div className="flex justify-center w-full desktop1:justify-start mb-[32px]">
                           <Button
@@ -169,17 +163,17 @@ export default function Influencer({ appDownloadButtons }) {
                             icon={<FaWhatsapp size={24} />}
                           />
                         </div>
-                        <MotionDivDownToUp>
-                          <div className="flex justify-center desktop1:justify-start">
-                            <div className="flex flex-col items-center desktop1:flex-row text-primary">
-                              {content.texts.hero.obsHero.icon}
-                              <p className="text-lighter  ml-[10px] text-start mt-[12px] desktop1:mt-0">
-                                {content.texts.hero.obsHero.text}
-                              </p>
-                            </div>
+
+                        <div className="flex justify-center desktop1:justify-start">
+                          <div className="flex flex-col items-center desktop1:flex-row text-primary">
+                            {content.texts.hero.obsHero.icon}
+                            <p className="text-lighter  ml-[10px] text-start mt-[12px] desktop1:mt-0">
+                              {content.texts.hero.obsHero.text}
+                            </p>
                           </div>
-                        </MotionDivDownToUp>
-                        {appDownloadButtons && (
+                        </div>
+
+                        {/* {appDownloadButtons && (
                           <MotionDivDownToUp>
                             <div className="flex flex-col items-center desktop1:items-start">
                               <p className="mb-[20px] mt-[60px] text-lighter  text-center">
@@ -228,7 +222,7 @@ export default function Influencer({ appDownloadButtons }) {
                               </div>
                             </div>
                           </MotionDivDownToUp>
-                        )}
+                        )} */}
                       </div>
                     </div>
                   </div>
